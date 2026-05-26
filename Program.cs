e6ce9dc8c3d6229f5b9a30349e97d7948d29cc71
Uzair Mohd Hanafiah <ac156549@avcol.school.nz>
2026-05-27 10:45:38 +1200

    I stored a string name variable and i also did a if method where whether or not the user wants to do a quiz.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace MaoriQuiz
+﻿using System.ComponentModel;
+
+namespace MaoriQuiz
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // User enters their name which is stored in the string name
+            Console.Write("Please Enter Your Name:" + " ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"I See, Your Name Is: {name}");
+
+            // User is asked a question whether he wants to do a quiz
+            Console.Write("Do You Want To Do A Quiz? (Yes Or No):" + " ");
+            string startQuestion = Console.ReadLine();
+
+            // If the output is not yes
+            if (startQuestion != "Yes")
+            {
+                Console.WriteLine("Okay");
+               
+            }
+            // If the outpout is yes
+            else
+            {
+                Console.WriteLine("Select Dificulty:\nEasy\nMedium\nHard\nImpossible");
+
+            }
+
         }
     }
 }
