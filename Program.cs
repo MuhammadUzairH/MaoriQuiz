47ee71228486faaed7fc4bda70cfc42154c3a1b6
Uzair Mohd Hanafiah <ac156549@avcol.school.nz>
2026-05-27 12:38:35 +1200

    Rewrote some bit of the code, Instead of doing the name method in the static void Main(string[] args) i just did it outside thanks from ArraySymbolMarks C#. Then i did some framework from the ArraySymbol and now i can finally input the question.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MaoriQuiz
 {
@@ -6,10 +7,15 @@ namespace MaoriQuiz
     {
         static void Main(string[] args)
         {
+            string name;
+            char quizDificultly,replay;
+
+            // Welcome page
+            displayWelcome("----------------------------");
+            
+            
             // User enters their name which is stored in the string name
-            Console.Write("Please Enter Your Name:" + " ");
-            string name = Convert.ToString(Console.ReadLine());
-            Console.WriteLine($"I See, Your Name Is: {name}");
+            name = inputName();
 
             // User is asked a question whether he wants to do a quiz
             Console.Write("Do You Want To Do A Quiz? (Yes Or No):" + " ");
@@ -21,12 +27,72 @@ namespace MaoriQuiz
                 Console.WriteLine("Okay");
 
             }
+            // Dsiplaying question and the difficulty the user sets it to be
             else
             {
+                do
+                {
+                    quizDificultly = displayChoice();
+                    switch (quizDificultly)
+                    {
+                        case 'E':
+                            {
+                                //the easiest quiz
 
-                Console.WriteLine("Select Dificulty:\nEasy\nMedium\nHard\nImpossible");
-                string asuvdkfauksfd = Console.ReadLine();
+                            }
+                            break;
+                        case 'M':
+                            {
+                                // the medium quiz
+
+                            }
+                            break;
+                        case 'H':
+                            {
+                                // the 2nd hardest quiz
+
+                            }
+                            break;
+                        case 'I':
+                            {
+                                // the most most hardest quiz
+
+                            }
+                            break;
+                        default:
+                            {
+                                Console.WriteLine("Invalid Option");
+                            }
+                            break;
+                    }
+                    Console.WriteLine("Congrast, You Got ");
+                    Console.WriteLine("Do You Want To Play Again? (Y/N)");
+                    replay = Console.ReadLine().ToUpper()[0];
+                } while (replay == 'Y');
             }
         }
+        // This is the welcome page
+        static void displayWelcome(string nsg) { Console.WriteLine("Welcome To The Quiz");
+            Console.WriteLine(nsg);
+                }
+        static string inputName() 
+        {
+            // This is the code where the user input their name
+            Console.Write("Please Enter Your First Name:" + " ");
+            string firstName = Convert.ToString(Console.ReadLine());
+            Console.Write("Please Enter Your First Name:" + " ");
+            string lastName = Convert.ToString(Console.ReadLine());
+            Console.WriteLine($"I See, Your Name Is: {firstName + lastName}");
+            string fullName = firstName + lastName;
+            return fullName;
+
+        }
+        static char displayChoice()
+        {
+            // This code allows user to choose between the dificulty they want to do
+            Console.WriteLine("Select Dificulty:\nEasy (E)\nMedium (M)\nHard (H)\nImpossible (I)");
+            char quizDificulty = Console.ReadLine().ToUpper()[0];
+            return quizDificulty;
+    }
     }
 }
