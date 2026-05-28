a3dff0123c4278b264240cbd0b8676493adc5d5c
Uzair Mohd Hanafiah <ac156549@avcol.school.nz>
2026-05-29 10:09:08 +1200

    I did some random stuff. Still needs fixing

--- Program.cs
+++ Program.cs
@@ -8,23 +8,23 @@ namespace MaoriQuiz
         static void Main(string[] args)
         {
             string name;
-            char quizDificultly,replay;
+            char quizDificultly,replay,choiceConfirmation;
 
             // Welcome page
             displayWelcome("----------------------------");
-            
-            
+
+
             // User enters their name which is stored in the string name
             name = inputName();
 
             // User is asked a question whether he wants to do a quiz
-            Console.Write("Do You Want To Do A Quiz? (Yes Or No):" + " ");
+            Console.Write("\nDo You Want To Do A Quiz? (Y/N):" + " ");
             string startQuestion = Console.ReadLine();
 
             // If the output is not yes
-            if (startQuestion != "Yes")
+            if (startQuestion != "Y")
             {
-                Console.WriteLine("Okay");
+                Console.WriteLine("\nOkay");
 
             }
             // Dsiplaying question and the difficulty the user sets it to be
@@ -38,36 +38,69 @@ namespace MaoriQuiz
                         case 'E':
                             {
                                 //the easiest quiz
+                                Console.WriteLine("\nAre You Sure You Want To Do Easy Mode? (Y/N):");
+                                choiceConfirmation = Convert.ToChar(Console.ReadLine());
+                                if (choiceConfirmation != 'Y')
+                                {
 
+
+                                }
                             }
                             break;
                         case 'M':
                             {
                                 // the medium quiz
+                                Console.WriteLine("\nAre You Sure You Want To Do Medium Mode? (Y/N):");
+                                choiceConfirmation = Convert.ToChar(Console.ReadLine());
+                                if (choiceConfirmation != 'Y')
+                                {
+                                    switch (quizDificultly)
+                                    {
+                                       
+                                    }
 
+                                }
                             }
                             break;
                         case 'H':
                             {
                                 // the 2nd hardest quiz
+                                Console.WriteLine("\nAre You Sure You Want To Do Hard Mode? (Y/N):");
+                                choiceConfirmation = Convert.ToChar(Console.ReadLine());
+                                if (choiceConfirmation != 'Y')
+                                {
+                                    switch (quizDificultly)
+                                    {
 
+                                    }
+
+                                }
                             }
                             break;
                         case 'I':
                             {
                                 // the most most hardest quiz
+                                Console.WriteLine("\nAre You Sure You Want To Do Impossible Mode? (Y/N):");
+                                choiceConfirmation = Convert.ToChar(Console.ReadLine());
+                                if (choiceConfirmation != 'Y')
+                                {
+                                    switch (quizDificultly)
+                                    {
 
+                                    }
+
+                                }
                             }
                             break;
                         default:
                             {
-                                Console.WriteLine("Invalid Option");
+                                Console.WriteLine("\nInvalid Option");
                             }
                             break;
                     }
                     Console.WriteLine("Congrast, You Got ");
                     Console.WriteLine("Do You Want To Play Again? (Y/N)");
-                    replay = Console.ReadLine().ToUpper()[0];
+                    replay = Console.ReadLine()[0];
                 } while (replay == 'Y');
             }
         }
@@ -78,11 +111,12 @@ namespace MaoriQuiz
         static string inputName() 
         {
             // This is the code where the user input their name
-            Console.Write("Please Enter Your First Name:" + " ");
+            Console.Write("\nPlease Enter Your First Name:" + " ");
             string firstName = Convert.ToString(Console.ReadLine());
-            Console.Write("Please Enter Your First Name:" + " ");
+            Console.Write("\nPlease Enter Your Last Name:" + " ");
             string lastName = Convert.ToString(Console.ReadLine());
-            Console.WriteLine($"I See, Your Name Is: {firstName + lastName}");
+            Console.WriteLine($"\nI See, Your Name Is: {firstName + lastName}");
+            Console.WriteLine("----------------------------");
             string fullName = firstName + lastName;
             return fullName;
 
@@ -90,7 +124,7 @@ namespace MaoriQuiz
         static char displayChoice()
         {
             // This code allows user to choose between the dificulty they want to do
-            Console.WriteLine("Select Dificulty:\nEasy (E)\nMedium (M)\nHard (H)\nImpossible (I)");
+            Console.WriteLine("\nSelect Dificulty:\nEasy (E)\nMedium (M)\nHard (H)\nImpossible (I)");
             char quizDificulty = Console.ReadLine().ToUpper()[0];
             return quizDificulty;
     }
